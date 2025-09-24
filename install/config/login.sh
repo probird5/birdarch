2e19b6186aba0c7d7f9099b89d2c4cf1d6a5a45d
probird5 <52969604+probird5@users.noreply.github.com>
2025-09-24 16:07:32 -0400

    updated script

--- install/config/login.sh
+++ install/config/login.sh
@@ -215,8 +215,8 @@ fi
 if [ ! -f /etc/systemd/system/birdarch-seamless-login.service ]; then
   cat <<EOF | sudo tee /etc/systemd/system/birdarch-seamless-login.service
 [Unit]
-Description=Omarchy Seamless Auto-Login
-Documentation=https://github.com/basecamp/birdarch
+Description=Birarch Seamless Auto-Login
+Documentation=https://github.com/basecamp/omarchy
 Conflicts=getty@tty1.service
 After=systemd-user-sessions.service getty@tty1.service plymouth-quit.service systemd-logind.service
 PartOf=graphical.target
